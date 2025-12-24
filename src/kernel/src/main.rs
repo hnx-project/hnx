0d490c2b809ea6af9fb8140a5edf875a670cc5cf
tinchychin97 <tinchychin97@gmail.com>
2025-12-24 17:26:11 +0800

    refactor(kernel): update kernel configuration and add device addresses; enhance linker script generation

--- src/kernel/src/main.rs
+++ src/kernel/src/main.rs
@@ -4,12 +4,12 @@
 mod panic;
 
 // mod arch;
-// mod core;
+// mod console;
 
 // use core::init::kernel_init;
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn rust_main() -> ! {
     // kernel_init();
     loop {}
 }
