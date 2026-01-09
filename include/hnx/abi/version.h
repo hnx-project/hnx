382c35525f0aa931dfea83e068e6bc528fde2291
tinchychin97 <tinchychin97@gmail.com>
2026-01-09 15:58:15 +0800

    refactor(kernel): 重构系统调用模块为子模块结构
    将系统调用实现从单个文件拆分为模块化结构，便于后续扩展和维护
    更新版本号和相关头文件中的版本信息

--- include/hnx/abi/version.h
+++ include/hnx/abi/version.h
@@ -4,7 +4,7 @@
 #define HNX_ABI_VERSION_MAJOR 0
 #define HNX_ABI_VERSION_MINOR 3
 #define HNX_ABI_VERSION_PATCH 0
-#define HNX_ABI_VERSION "0.3.0-alpha.1+20260109.8aca1fb"
+#define HNX_ABI_VERSION "0.3.0-alpha.1+20260109.0d65f7d"
 
 // 检查兼容性的宏
 #define HNX_ABI_CHECK_VERSION(major, minor, patch) \
